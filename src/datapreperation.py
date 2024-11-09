e63ef12a44bb975886f1cb284e53a35e68a3f3c2
sasankjampana8 <sasankjampana8@gmail.com>
2024-11-09 09:58:53 +0530

    smallupdates

--- src/datapreperation.py
+++ src/datapreperation.py
@@ -13,7 +13,18 @@ class DataPrep:
         self.X_train, self.X_val, self.Y_train, self.Y_val = train_test_split(X, Y, test_size=0.3, random_state=42)
     
     
+    def label_encode(self,):
+        encoder = LabelEncoder()
+        
+    def one_hot_encode(self,):
+        encoder = OneHotEncoder()
+        
+    def home_market_value(self,):
+        pass
     
+    def date_col(self,):
+        self.X_train
+        self.X_val
     
     
     def preprocess(self,):
